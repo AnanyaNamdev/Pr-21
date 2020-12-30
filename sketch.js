505dd4bcfd76ab9db1907d461ab24be4ea0d79da
AnanyaNamdev <75675325+AnanyaNamdev@users.noreply.github.com>
2020-12-30 21:25:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var bullet;
+var wall;
+var speed, weight, thickness;
+var bulletRightEdge, wallLeftEdge;
+
+function setup() {
+  createCanvas(1600,400);
+
+speed = random(223, 321);
+weight = random(30, 52);
+thickness = random(22, 83);
+
+bullet = createSprite(50, 200, 100, 10);
+bullet.shapeColor = "white";
+bullet.velocityX = speed;
+
+wall = createSprite(1200, 200, thickness, height / 2);
+wall.shapeColor = 80, 80, 80;
+
+}
+
+function draw() {
+  background(0); 
+  
+  if(hasCollided(bullet, wall)) {
+
+bullet.velocityX = 0;
+
+var damage = 0.5 * weight * speed * speed / (thickness * thickness * thickness);
+
+if(damage < 10) {
+   wall.shapeColor = "green";
+}
+
+if(damage > 10) {
+   wall.shapeColor = "red";
+}
+
+  }
+
+  drawSprites();
+}
